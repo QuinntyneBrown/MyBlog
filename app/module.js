0494f58f21d294116a5a6c426a0e501ff440c9f0
Quinntyne Brown <quinntynebrown@gmail.com>
2014-11-19 02:17:24 -0500

    initial code
    initial code

---
+++ app/module.js
@@ -0,0 +1,103 @@
+﻿(function () {
+
+    "use strict";
+
+    var app = angular.module("app", ["ngRoute"]);
+
+
+    app.config(["$routeProvider", function ($routeProvider) {
+
+        $routeProvider
+
+        .when("/",
+        {
+            templateUrl: 'app/views/list.html'
+        })
+        .when("/post/:index",
+        {
+            templateUrl: 'app/views/post.html'
+        })
+        .otherwise("/");
+
+    }]);
+
+})();
+
+
+(function () {
+
+    "use strict";
+
+    var serviceId = "blogService";
+
+    angular.module("app").service(serviceId, ["$http", "$q", service]);
+
+    function service($http, $q) {
+
+        var self = this;
+
+        self.getPosts = function () {
+            return $q.when([
+                { name: "Rememberance Day Post", body: "Great to Remember!" },
+                { name: "Chistmas Is Next", body: "Yes!" }
+            ]);
+
+            //return $http({ method: "GET", url: "/api/post/getall" }).then(function (results) {
+            //    return results;
+            //});
+        };
+
+        return self;
+    };
+
+})();
+
+
+(function () {
+
+    "use strict";
+
+    var controllerId = "blogController";
+
+    angular.module("app").controller(controllerId, ["blogService",controller]);
+
+    function controller(blogService) {
+
+        var self = this;
+
+        blogService.getPosts().then(function (results) {
+
+            self.posts = results;
+
+        });
+
+        return self;
+
+    };
+
+})();
+
+
+(function () {
+
+    "use strict";
+
+    var controllerId = "postController";
+
+    angular.module("app").controller(controllerId, ["blogService", "$routeParams", controller]);
+
+    function controller(blogService, $routeParams) {
+
+        var self = this;
+
+        blogService.getPosts().then(function (results) {
+
+            self.post = results[$routeParams.index];
+
+        });
+
+        return self;
+
+    };
+
+})();
